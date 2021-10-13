07b50110a0eeb38c266f860612593637dacd6c93
Alex Fritz <alexfritzcoach@gmail.comm>
2021-10-13 14:58:20 -0700

    Pass second test

--- src/book.js
+++ src/book.js
@@ -1,10 +1,21 @@
 function createTitle(title) {
   return `The ${title}`;
 }
+function buildMainCharacter(name, age, pronouns) {
+  var mainCharacter = {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+  return mainCharacter;
+};
+function saveReview() {
+
+}
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
