75e40d5599503a3467a3aa209965699d0c81d520
Alex Fritz <alexfritzcoach@gmail.comm>
2021-10-14 19:05:26 -0700

    Finish Library tests

--- src/Library.js
+++ src/Library.js
@@ -14,9 +14,20 @@ function addBook(library, book) {
   library.shelves[book.genre].push(book);
 }
 
+function checkoutBook(library, title, shelf) {
+  var noCheckOut = `Sorry, there are currently no copies of ${title} available at the ${library.name}`;
+  var checkOut = `You have now checked out ${title} from the ${library.name}`;
+  for (var i = 0; i < library.shelves[shelf].length; i++) {
+    if (title === library.shelves[shelf][i].title) {
+      library.shelves[shelf].splice(i, 1);
+      return checkOut;
+    }
+  }
+  return noCheckOut;
+}
 
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
+  checkoutBook
 };
