e1c659f452264804c3690b7d59aec6e905e2da10
Alex Fritz <alexfritzcoach@gmail.comm>
2021-10-17 17:42:39 -0700

    Pass librarian class tests

--- src/librarian.js
+++ src/librarian.js
@@ -2,7 +2,7 @@ class Librarian {
   constructor(name, library) {
     this.name = name;
     this.library = library;
-  }
+  };
   greetPatron(name, ifMorning) {
     var morning = ifMorning;
     if (morning === true) {
@@ -10,11 +10,31 @@ class Librarian {
     } else {
       return `Hello, ${name}!`;
     }
+  };
+  findBook(book) {
+    for(var i = 0; i < this.library.shelves.fantasy.length; i++) {
+      if (book === this.library.shelves.fantasy[i].title) {
+        this.library.shelves.fantasy.splice(i, 1)
+        return `Yes, we have ${book}`;
+      }
+    }
+    for(var i = 0; i < this.library.shelves.nonFiction.length; i++) {
+      if (book === this.library.shelves.nonFiction[i].title) {
+        this.library.shelves.nonFiction.splice(i, 1)
+        return `Yes, we have ${book}`;
+      }
+    }
+    for(var i = 0; i < this.library.shelves.fiction.length; i++) {
+      if (book === this.library.shelves.fiction[i].title) {
+        this.library.shelves.fiction.splice(i, 1)
+        return `Yes, we have ${book}`;
+      }
+    }
+    return `Sorry, we do not have ${book}`;
   }
-
   calculateLateFee(fee) {
     return Math.ceil(fee * .25);
   }
-}
+};
 
 module.exports = Librarian;
